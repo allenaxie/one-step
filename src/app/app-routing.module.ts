53a1a23cdac9220fbcff4be05b9f8c1b87a9ca77
allenaxie <allenaxie1@gmail.com>
2023-09-20 20:42:23 +0800

    fix: root path component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/shared/home/home.component';
 
-const routes: Routes = [{ path: '', component: AppComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
