48e3c17c6946a6c1643a0af891111f941fa1a7c3
allenaxie <allenaxie1@gmail.com>
2023-09-23 18:47:46 +0800

    feat: Update home component. Add reusable scss and tailwind css..
    - Changed background image in home component.
    - Added custom tailwind css styles in tailwind.config.
    - Added fonts file.
    - Added reusable animations file.
    - Added reusable color filters file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "40%": "40%",
+      },
+      fontFamily: {
+        zcoolXiaoWei: ["ZCOOL_XiaoWei"],
+      },
+      transitionDelay: {
+        1000: "1000ms",
+        2000: "2000ms",
+        3000: "3000ms",
+        4000: "4000ms",
+        5000: "5000ms",
+      },
+      transitionDuration: {
+        1000: "1000ms",
+        2000: "2000ms",
+        3000: "3000ms",
+        4000: "4000ms",
+        5000: "5000ms",
+      },
+    },
   },
   plugins: [],
 };
