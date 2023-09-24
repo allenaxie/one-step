e379ad1c7b8c5ac06d041ebd2cd8ff7d2d04bbf2
allenaxie <allenaxie1@gmail.com>
2023-09-24 15:52:02 +0800

    fix: update structure between nav bar and main content.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,28 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
     extend: {
+      height: {
+        "screen-10": "10vh",
+        "screen-20": "20vh",
+        "screen-30": "30vh",
+        "screen-40": "40vh",
+        "screen-50": "50vh",
+        "screen-60": "60vh",
+        "screen-70": "70vh",
+        "screen-80": "80vh",
+        "screen-90": "90vh",
+      },
+      width: {
+        "screen-10": "10vw",
+        "screen-20": "20vw",
+        "screen-30": "30vw",
+        "screen-40": "40vw",
+        "screen-50": "50vw",
+        "screen-60": "60vw",
+        "screen-70": "70vw",
+        "screen-80": "80vw",
+        "screen-90": "90vw",
+      },
       spacing: {
         "40%": "40%",
         "50%": "50%",
